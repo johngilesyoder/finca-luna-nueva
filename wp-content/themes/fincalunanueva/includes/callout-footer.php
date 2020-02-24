a9dd2d3aa2afc60906224cad1c0ad023f2c13b2c
John Yoder <johngilesyoder@gmail.com>
2020-02-24 12:31:31 -0700

    Update reservations button link

--- wp-content/themes/fincalunanueva/includes/callout-footer.php
+++ wp-content/themes/fincalunanueva/includes/callout-footer.php
@@ -5,7 +5,7 @@
         <div class="statement">
           <?php the_field('callout_text', 'option'); ?>
         </div>
-        <a href="https://reservations.orbebooking.com/Search/SearchAvailability/VYPVA//en/undefined/undefined/2/0" class="btn btn-primary">Reserve now</a>
+        <a href="https://reservations.orbebooking.com/Search/Init/VYPVA" class="btn btn-primary">Reserve now</a>
         <span class="or">or</span>
         <a href="/contact-us" class="btn btn-secondary">Contact us</a>
         <p class="retreats">Are you an educator or business interested in our Workshops and Retreats? <a href="/accommodations/workshops-retreats">Find out more &rarr;</a></p>
