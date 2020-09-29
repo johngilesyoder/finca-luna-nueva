9d7f25b97a967efdf512df68a90becdefaeec431
John Yoder <johngilesyoder@gmail.com>
2020-09-29 12:40:03 -0600

    Add insta feed to homepage

--- wp-content/themes/fincalunanueva/includes/home/instagram.php
+++ wp-content/themes/fincalunanueva/includes/home/instagram.php
@@ -1,7 +1,7 @@
 <div class="container">
   <section class="home-instagram">
     <div class="journal-title">
-      <h2>We're on Instagram</h2>
+      <h2>FLN on Instagram</h2>
     </div>
     <?php echo do_shortcode( '[instagram feed="2276"]' ); ?>
   </section>
